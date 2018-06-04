4e099bac0677fa2bd7189f724061334a458d9258
Ryan <ryan.lam@amox.hk>
2018-06-04 17:07:17 +0800

    update

--- PHP-common-functions.php
+++ PHP-common-functions.php
@@ -9,6 +9,23 @@ function SearchArrayByValue ($SearchArray, $SearchKey, $SearchValue) {
 } // END OF -function SearchArrayByValue ($SearchArray, $SearchKey, $SearchValue) {-
 // $Result = SearchArrayByValue($CustArray, 'ID', '001');
 
+
+/*	multidimensional array sort by value, 2018-06-04
+	$SortOrder='SORT_DESC'
+	Return: sorted array()
+*/
+function SortArrayByValue ($SortArray, $SortKey, $SortOrder='SORT_ASC') {
+    // Obtain a list of columns
+    foreach ($SortArray as $key => $row) {
+        $Sort[$key] = $row[$SortKey];
+    }
+    array_multisort($Sort, $SortOrder, $SortArray);
+    return $SortArray;
+} // END OF -function SortArrayByValue ($SortArray, $SortKey, $SortValue) {-
+// $Result = SortArrayByValue($CustArray, 'name');
+
+
+
 // e.g.
 $userdb = array(
     array(
@@ -28,11 +45,17 @@ $userdb = array(
     )
 );
 
+/*
 $Result = SearchArrayByValue($userdb, 'uid', '40489');
 if( empty($Result) ) {
     print "No result match";
 } else {
     print_r($Result);
 }
+*/
+print_r($userdb);
+
+print_r(SortArrayByValue($userdb, 'name'));
+
 
 ?>
--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Customized common functions for php
 ## multidimensional array search by value (2018-05-20)
 
 function SearchArrayByValue ($SearchArray, $SearchKey, $SearchValue)
+
+## multidimensional array sorting by value (2018-06-04)
+
+function SortArrayByValue ($SortArray, $SortKey, $SortValue)
